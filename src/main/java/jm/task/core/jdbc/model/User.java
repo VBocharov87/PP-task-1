f43dc3dece27ea6e6d492e11b87cfcfa806b5181
Vladimir Bocharov <v.v.bocharov.87@yandex.ru>
2024-08-05 18:18:51 +0300

    PP-Task-1 (user changing)

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,8 +1,6 @@
 package jm.task.core.jdbc.model;
 
-import java.io.Serializable;
-
-public class User implements Serializable {
+public class User {
 
     private Long id;
     private String name;
